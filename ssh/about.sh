5dc6472510110286f4237faf218af85b0e45d730
scvps69 <123447615+scvps69@users.noreply.github.com>
2023-01-24 17:20:52 +0700

    Add files via upload

---
+++ ssh/about.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+clear
+echo -e "================================================="
+echo -e "#     Premium Auto Script Mod By VPS69          #"
+echo -e "================================================="
+echo -e "# For Debian 10 64 bit                          #"
+echo -e "# For Ubuntu 18.04 & Ubuntu 20.04 64 bit        #"
+echo -e "# For VPS with KVM and VMWare virtualization    #"
+echo -e "================================================="
+echo -e "# My Company                                    #"
+echo -e "================================================="
